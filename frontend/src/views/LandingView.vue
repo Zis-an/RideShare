bbf6c949f0afcf15b80110a6dc5b66f3ed1c3b68
Zis-an <zahid.officialuse@gmail.com>
2024-05-21 18:11:50 +0600

    Problem exists

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -5,7 +5,9 @@
             <div class="bg-white px-4 py-5 sm:p-6">
                 <div class="flex justify-between">
                     <button class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Start Driving</button>
-                    <button class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Find A Ride
+                    <button 
+                    @click="handleFindARide"
+                    class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Find A Ride
                     </button>
                 </div>
             </div>
@@ -14,5 +16,12 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+const handleFindARide = () => {
+    router.push({
+        name: 'location'
+    })
+}
 </script>
