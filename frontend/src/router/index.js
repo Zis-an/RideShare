175307bdebdb7c4fb3d430533311f3e4eb62bf52
Zis-an <zahid.officialuse@gmail.com>
2024-05-21 11:30:25 +0600

    Second Commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import LandingView from '../views/LandingView.vue'
 
