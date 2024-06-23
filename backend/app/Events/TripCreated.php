f2632f9f0cd5e9089d38abdf9386013798485796
Zis-an <zahid.officialuse@gmail.com>
2024-06-23 18:31:11 +0600

    Maybe Solved Pusher

--- backend/app/Events/TripCreated.php
+++ backend/app/Events/TripCreated.php
@@ -12,7 +12,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class TripCreated
+class TripCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- frontend/src/views/StandbyView.vue
+++ frontend/src/views/StandbyView.vue
@@ -1,8 +1,25 @@
 <template>
     <div class="pt-16">
-        <h1 class="text-3xl font-semibold mb-4">Waiting for ride request...</h1>
-        <div class="mt-8 flex justify-center">
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
+        <div v-if="!trip.id" class="mt-8 flex justify-center">
             <Loader />
+        </div>
+        <div v-else>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <GMapMap :zoom="14" :center="trip.destination" ref="gMap"
+                            style="width:100%; height: 256px;"></GMapMap>
+                    </div>
+                    <div class="mt-2">
+                        <p class="text-xl">Going to <strong>{{ trip.destination_name }}</strong></p>
+                    </div>
+                </div>
+                <div class="flex justify-between bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Decline</button>
+                    <button class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Accept</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -14,6 +31,14 @@ import Loader from '@/components/Loader.vue'
 import { onMounted } from 'vue'
 import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
+import { useTripStore } from '@/stores/trip'
+
+
+
+
+const title = ref('Waiting for ride request....')
+const trip = useTripStore()
+
 
 onMounted(() => {
 
@@ -24,14 +49,17 @@ onMounted(() => {
         broadcaster: 'pusher',
         key: '131a5f51e691a523e3a6',
         cluster: 'ap1',
-        forceTls: false,
+        wsHost: window.location.hostname,
+        wsPort: 6001,
+        forceTLS: false,
         disableStats: true,
         enabledTransports: ['ws', 'wss']
     })
 
+
     // Debugging: Log connection state changes
     echo.connector.pusher.connection.bind('state_change', (states) => {
-        console.log(states);
+        console.log('Connection state changed:', states);
     });
 
     // Debugging: Log errors
@@ -41,6 +69,9 @@ onMounted(() => {
 
     echo.channel('drivers')
         .listen('TripCreated', (e) => {
+            title.value = 'Ride requested:'
+   
+            trip.$patch(e.trip)
             console.log('TripCreated', e)
         })
 
